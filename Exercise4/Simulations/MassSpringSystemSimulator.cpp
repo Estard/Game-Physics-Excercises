b0cb034e45b35b6f7fbb97d89c9785d65fd63ef7
Unknown <estardastrius@gmail.com>
2020-01-16 13:59:20 +0100

    Prepare Exercise 4

---
+++ Exercise4/Simulations/MassSpringSystemSimulator.cpp
@@ -0,0 +1,369 @@
+#include "MassSpringSystemSimulator.h"
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+
+struct MassPoint
+{
+    Vec3 position,velocity;
+    bool isFixed;
+    Vec3 force;
+    double mass;
+};
+struct Spring
+{
+    int massPoint1,massPoint2;
+    double initialLength;
+    double stiffness;
+};
+
+
+std::vector<MassPoint> massPoints;
+std::vector<Spring> springs;
+std::vector<Vec3> leap;
+int methode = EULER;
+double internTimestep = -1;
+bool gravitation = false;
+bool collision = false;
+float bounciness = 0.0;
+
+std::string vec3ToString(Vec3 &v)
+{
+    std::stringstream ss;
+    ss << v.x << ' ' << v.y <<' ' << v.z;
+    return ss.str();
+}
+std::string massPointToString(MassPoint &ms)
+{
+ std::stringstream ss;
+ ss << "Mass Point, isFixed : " << ms.isFixed;
+ ss << "\nPosition: "<<vec3ToString(ms.position);
+ ss << "\nVelocity: "<<vec3ToString(ms.velocity);
+ return ss.str();
+}
+std::string springToString(Spring &s)
+{
+    std::stringstream ss;
+    ss << "String initial length: "<<s.initialLength;
+    ss << "\nMassPoint1:\n"<<massPointToString(massPoints[s.massPoint1]);
+    ss << "\nMassPoint2:\n"<<massPointToString(massPoints[s.massPoint2]);
+    return ss.str();
+    
+
+}
+
+
+MassSpringSystemSimulator::MassSpringSystemSimulator()
+{
+     m_fMass = 1.;
+	 m_fStiffness = 1.;
+	 m_fDamping = 0.;
+	 m_mouse = Point2D();
+	 m_trackmouse = Point2D();
+	 m_oldtrackmouse = Point2D();
+	 m_iIntegrator = 0;
+	 springs.clear();
+	 massPoints.clear();
+	 leap.clear();
+}
+
+void applyForces(std::vector<Spring> &springs, std::vector<MassPoint> &massPoints, double damping)
+{    
+    //Spring Forces
+    for(auto &s: springs){
+        MassPoint &m1 = massPoints[s.massPoint1];
+        MassPoint &m2 = massPoints[s.massPoint2];
+        double lengthDif = sqrt(m1.position.squaredDistanceTo(m2.position)) -s.initialLength;
+        double totalForce = s.stiffness*lengthDif;
+        Vec3 dir = m2.position-m1.position;
+        normalize(dir);
+        m1.force += dir*totalForce;
+        m2.force -= dir*totalForce;
+    }
+	for (auto& m : massPoints) {
+		m.force += -damping * m.velocity;
+	}
+}
+void EulerIntegration(std::vector<Spring> &springs,std::vector<MassPoint> &mps,double timestep)
+{
+    for(auto &ms: mps)
+    {
+		if (!ms.isFixed)
+		{
+			ms.position += timestep * ms.velocity;
+			ms.velocity += ms.force * (timestep / ms.mass);
+		}
+    }
+}
+void MidpointIntegration(std::vector<Spring> &springs,std::vector<MassPoint> &mps,double timestep, double damping)
+{
+    std::vector<MassPoint> midPointMP = mps;
+
+    EulerIntegration(springs, midPointMP,timestep*.5);
+    applyForces(springs, midPointMP, damping);
+    for(size_t i = 0; i < mps.size();i++){
+		if (!mps[i].isFixed)
+		{
+			mps[i].position += timestep * midPointMP[i].velocity;
+			mps[i].velocity += timestep * midPointMP[i].force / mps[i].mass;
+		}
+    }    
+}
+void LeapFrogIntegration(std::vector<Spring>& springs, std::vector<MassPoint>& mps, double timestep, double damping) 
+{
+	for (int i = 0; i < mps.size(); i++)
+	{
+		mps[i].velocity = mps[i].force * (timestep / mps[i].mass) + leap[i];
+		if(!mps[i].isFixed)
+			mps[i].position += timestep * mps[i].velocity;
+		leap[i] = mps[i].velocity;
+	}
+}
+
+ 
+
+void MassSpringSystemSimulator::simulateTimestep(float timeStep)
+{
+	if(!massPoints.empty() && massPoints[0].mass != m_fMass)
+		setMass(m_fMass);
+
+	if(!(internTimestep < 0) )
+		timeStep = internTimestep;
+	//Apply Forces
+	for (auto& ms : massPoints)
+	{
+		ms.force = Vec3();
+	}
+	applyForces(springs, massPoints, m_fDamping);
+	
+	//Integrate based on Method
+	switch (methode)
+	{
+	case EULER:
+		leap.clear();
+		EulerIntegration(springs, massPoints, timeStep);
+		break;
+	case MIDPOINT:
+		leap.clear();
+		MidpointIntegration(springs, massPoints, timeStep, m_fDamping);
+		break;
+	case LEAPFROG:
+		if (leap.size() == 0) {
+			for (auto& ms : massPoints)
+			{
+				leap.push_back(ms.velocity);
+			}
+		}
+		LeapFrogIntegration(springs, massPoints, timeStep, m_fDamping);
+		break;
+	default:
+		break;
+	}
+	//Collision with GroundPlane
+	if (collision)
+	{
+		for (int i = 0; i<getNumberOfMassPoints(); i++)
+		{
+			if (massPoints[i].position.y < -1) {
+				massPoints[i].position.y = -1;
+				massPoints[i].velocity.y *= -bounciness; //*= -1 für bounciness
+				if(methode == LEAPFROG)
+					leap[i].y *= -bounciness;
+			}
+		}
+	}
+	for (auto &s : springs) {
+		std::cout << "Updates Springs: " << springToString(s) << std::endl;
+	}
+	std::cout << getNumberOfSprings() << std::endl;
+}
+		/*Boring Getters and Setters*/
+		void MassSpringSystemSimulator::setMass(float mass)
+		{
+			m_fMass = mass;
+			for (auto &ms : massPoints)
+				ms.mass = m_fMass;
+
+		}
+		void MassSpringSystemSimulator::setStiffness(float stiffness)
+		{
+			m_fStiffness = stiffness;
+			for (auto &s : springs)
+				s.stiffness = m_fStiffness;
+		}
+		void MassSpringSystemSimulator::setDampingFactor(float damping)
+		{
+			m_fDamping = damping;
+		}
+		int MassSpringSystemSimulator::addMassPoint(Vec3 position, Vec3 Velocity, bool isFixed)
+		{
+			MassPoint mp = { position,Velocity,isFixed,Vec3(),m_fMass };
+			massPoints.push_back(mp);
+			return getNumberOfMassPoints() -1;
+		}
+		void MassSpringSystemSimulator::addSpring(int masspoint1, int masspoint2, float initialLength)
+		{
+			Spring s = { masspoint1,masspoint2,initialLength, m_fStiffness };
+			springs.push_back(s);
+		}
+		int MassSpringSystemSimulator::getNumberOfMassPoints()
+		{
+			return static_cast<int>(massPoints.size());
+		}
+		int MassSpringSystemSimulator::getNumberOfSprings()
+		{
+			return static_cast<int>(springs.size());
+		}
+		Vec3 MassSpringSystemSimulator::getPositionOfMassPoint(int index)
+		{
+			if (index <= massPoints.size())
+				return massPoints[index].position;
+			else
+				return Vec3();
+		}
+		Vec3 MassSpringSystemSimulator::getVelocityOfMassPoint(int index)
+		{
+			if (index <= massPoints.size())
+				return massPoints[index].velocity;
+			else
+				return Vec3();
+		}
+		void MassSpringSystemSimulator::applyExternalForce(Vec3 force)
+		{
+			m_externalForce = force;
+		}
+
+		void MassSpringSystemSimulator::onClick(int x, int y)
+		{
+			m_trackmouse.x = x;
+			m_trackmouse.y = y;
+		}
+
+		void MassSpringSystemSimulator::onMouse(int x, int y)
+		{
+			m_oldtrackmouse.x = x;
+			m_oldtrackmouse.y = y;
+			m_trackmouse.x = x;
+			m_trackmouse.y = y;
+		}
+
+		void MassSpringSystemSimulator::notifyCaseChanged(int testCase)
+		{
+			m_iTestCase = testCase;
+		}
+
+		void MassSpringSystemSimulator::externalForcesCalculations(float timeElapsed)
+		{
+				for (auto& m : massPoints)
+				{
+					if(gravitation)
+						m.velocity += 9.8 * Vec3(0, -1, 0) * timeElapsed;
+				}
+		}
+
+		const char* MassSpringSystemSimulator::getTestCasesStr()
+		{
+			return "Demo1,Demo2,Demo3,Demo4,Demo5";
+		}
+
+		void MassSpringSystemSimulator::initUI(DrawingUtilitiesClass * DUC)
+		{
+			this->DUC = DUC;
+			springs.clear();
+			massPoints.clear();
+			m_fDamping = 0.;
+
+			setMass(10);
+			setStiffness(40);
+			gravitation = false;
+			collision = false;
+			if(m_iTestCase < 3){
+				int m0 = addMassPoint(Vec3(0, 0, 0), Vec3(1, 0, 0), false);
+				int m1 = addMassPoint(Vec3(0, 2, 0), Vec3(-1, 0, 0), false);
+				addSpring(m0, m1, 1);
+			}
+			else if(m_iTestCase == 3)
+			{
+				addMassPoint(Vec3(-4, 2, 0), Vec3(0, -1, 0), false);
+				addMassPoint(Vec3(-3, 2, 0), Vec3(1, 0, 0), false);
+				addMassPoint(Vec3(-2, 2, 0), Vec3(0, 0, -1), false);
+				addMassPoint(Vec3(-3, 0, 0), Vec3(-1, 0, 0), false);
+				addMassPoint(Vec3(-1, 2, 0), Vec3(0, 0, 1), false);
+				addMassPoint(Vec3(-1, 0, 0), Vec3(1, 0, 0), false);
+				addMassPoint(Vec3(1, 0, 0), Vec3(), false);
+				addMassPoint(Vec3(1, 2, 0), Vec3(1, 0, 0), false);
+				addMassPoint(Vec3(2, 0, 0), Vec3(0, 3, 0), false);
+				addMassPoint(Vec3(2, 2, 0), Vec3(), true);
+				addMassPoint(Vec3(3, 0, 0), Vec3(), false);
+				addMassPoint(Vec3(4, 2, 0), Vec3(), false);
+				addMassPoint(Vec3(4, 0, 0), Vec3(), false);
+
+				addSpring(0, 1, 0.5);
+				addSpring(1, 2, 0.5);
+				addSpring(1, 3, 1);
+				addSpring(4, 5, 1);
+				addSpring(5, 6, 1);
+				addSpring(6, 7, 1);
+				addSpring(8, 9, 1);
+				addSpring(9, 10, 1);
+				addSpring(10, 11, 1);
+				addSpring(11, 12, 1);
+			}
+			else
+			{
+				addMassPoint(Vec3(), Vec3(), false);
+				addMassPoint(Vec3(0, 2, 0), Vec3(), true);
+				addMassPoint(Vec3(1, 0, 0), Vec3(-1, 0, 0), false);
+				addSpring(0, 1, 1);
+				addSpring(0, 2, 2);
+			}
+			methode = EULER;
+			switch (m_iTestCase)
+			{
+				case 0:
+					internTimestep = .1;
+				break;
+				case 2:
+					methode = MIDPOINT;
+				case 1:
+					internTimestep = .005;
+				break;
+				case 4:
+					methode = LEAPFROG;
+				default:
+					internTimestep = -1;
+					TwAddVarRW(DUC->g_pTweakBar, "Simulation Methode", TW_TYPE_INT32, &methode, "min=0 max=2");
+					TwAddVarRW(DUC->g_pTweakBar, "Damping", TW_TYPE_FLOAT, &m_fDamping, "min=0.0");
+					TwAddVarRW(DUC->g_pTweakBar, "Collision", TW_TYPE_BOOLCPP, &collision, "");
+					TwAddVarRW(DUC->g_pTweakBar, "Bounciness", TW_TYPE_FLOAT, &bounciness, "min=0.0");
+					TwAddVarRW(DUC->g_pTweakBar, "Mass", TW_TYPE_FLOAT, &m_fMass, "min=0.001");
+					TwAddVarRW(DUC->g_pTweakBar, "Gravitation", TW_TYPE_BOOLCPP, &gravitation, "");
+				break;
+			}			
+		}
+
+		void MassSpringSystemSimulator::reset()
+		{
+			m_mouse.x = m_mouse.y = 0;
+			m_trackmouse.x = m_trackmouse.y = 0;
+			m_oldtrackmouse.x = m_oldtrackmouse.y = 0;
+		}
+
+		void MassSpringSystemSimulator::drawFrame(ID3D11DeviceContext* pd3dImmediateContext)
+		{
+			DUC->setUpLighting(Vec3(), 0.4 * Vec3(1, 1, 1), 100, 0.6 * Vec3(0.97, 0.86, 1));
+			for (auto& mp : massPoints)
+			{
+				DUC->drawSphere(mp.position, Vec3(.05, .05, .05));
+			}
+			for (auto& s : springs)
+			{
+				DUC->beginLine();
+				DUC->drawLine(massPoints[s.massPoint1].position, Vec3(0., 1., 1.),
+					massPoints[s.massPoint2].position, Vec3(0., 1., 1.));
+				DUC->endLine();
+			}
+		}
+	
---
+++ Exercise4/Simulations/RigidBodySystemSimulator.cpp
@@ -0,0 +1,196 @@
+#include "RigidBodySystemSimulator.h"
+
+
+#include <vector>
+
+
+struct RigidBody
+{
+	Vec3 position = Vec3();
+	Vec3 scale = Vec3();
+	Quat rotation = Quat(Vec3(1,0,0),0);
+	Vec3 linearVelocity = Vec3();
+	Vec3 angularVelocity = Vec3();
+	Vec3 angularMomentum = Vec3();
+	Vec3 force = Vec3();
+	Vec3 torque = Vec3();
+	Vec3 inverseInertia = Vec3(); //Diagonal of matrix
+	float mass = 1.;
+};
+
+std::vector<RigidBody> rigidBodies;
+
+Vec3 calcInvInertiaCube(Vec3 size, float mass)
+{
+	float factor = mass / 12.;
+	float w2 = size.x * size.x;
+	float h2 = size.y * size.y;
+	float d2 = size.z * size.z;
+	return Vec3(1. / (factor * (double)(h2 + d2)),
+		1. / (factor * (double)(w2 + d2)),
+		1. / (factor * (double)(h2 + w2)));
+}
+
+// Construtors
+RigidBodySystemSimulator::RigidBodySystemSimulator() {
+	rigidBodies.clear();
+}
+
+// Functions
+const char* RigidBodySystemSimulator::getTestCasesStr()
+{
+	return "demo1,demo2,demo3,demo4";
+}
+void RigidBodySystemSimulator::initUI(DrawingUtilitiesClass* DUC)
+{
+	this->DUC = DUC;
+	rigidBodies.clear();
+	switch (m_iTestCase) {
+	case 0:
+		addRigidBody(Vec3(), Vec3(1., .6, .5), 2.);
+		setOrientationOf(0, Quat(Vec3(0, 0, 1), M_PI * .5));
+		applyForceOnBody(0, Vec3(.3, .5, .25), Vec3(1, 1, 0));
+		simulateTimestep(1);
+		{
+		auto lin_vel = getLinearVelocityOfRigidBody(0);
+		auto ang_vel = getAngularVelocityOfRigidBody(0);
+		//auto worldspc_vel = lin_vel + 
+		std::cout << "[Linear Velocity] x: " << lin_vel.x << " y: " << lin_vel.y << " z: " << lin_vel.z << "\n";
+		std::cout << "[Angular Velocity] x: " << ang_vel.x << " y: " << ang_vel.y << " z: " << ang_vel.z << "\n";
+		std::cout << "[World Velocity] ToDo\n";
+		}
+		break;
+	case 1:
+		addRigidBody(Vec3(), Vec3(1., .6, .5), 2.);
+		setOrientationOf(0, Quat(Vec3(0, 0, 1), M_PI * .5));
+		applyForceOnBody(0, Vec3(.3, .5, .25), Vec3(1, 1, 0));
+		simulateTimestep(0.01);
+		break;
+	case 2:
+		addRigidBody(Vec3(), Vec3(1., .6, .5), 2.);
+		addRigidBody(Vec3(2,2,2), Vec3(1., .6, .5), 2.);
+		setVelocityOf(1,Vec3(-1,-1,-1));
+		setOrientationOf(1,Quat(Vec3(1,1,0),M_PI*.5));
+	break;
+
+	case 3:
+		addRigidBody(Vec3(), Vec3(1., .6, .5), 2.);
+		addRigidBody(Vec3(2,2,2), Vec3(1., .6, .5), 2.);
+		addRigidBody(Vec3(5,0,0), Vec3(1., .6, .5), 2.);
+		addRigidBody(Vec3(-2,-2,-2), Vec3(1., .6, .5), 2.);
+		setVelocityOf(1,Vec3(-1,-1,-1));
+		setVelocityOf(3,Vec3(1,1,1));
+		setVelocityOf(2,Vec3(-1,0,0));
+		setOrientationOf(1,Quat(Vec3(1,1,0),M_PI*.5));
+	break;
+	}
+}
+void RigidBodySystemSimulator::reset()
+{
+	rigidBodies.clear();
+}
+void RigidBodySystemSimulator::drawFrame(ID3D11DeviceContext* pd3dImmediateContext)
+{
+	for (auto& rb : rigidBodies) {
+		auto rot = rb.rotation.getRotMat();
+		Mat4 scale;
+		scale.initScaling(rb.scale.x, rb.scale.y, rb.scale.z);
+		Mat4 translate;
+		translate.initTranslation(rb.position.x, rb.position.y, rb.position.z);
+		DUC->drawRigidBody(scale * rot * translate);
+	}
+
+	//DUC->drawSphere(Vec3(0, 0, 2), Vec3(0.05, 0.05, 0.05));
+}
+void RigidBodySystemSimulator::notifyCaseChanged(int testCase)
+{
+	m_iTestCase = testCase;
+}
+void RigidBodySystemSimulator::externalForcesCalculations(float timeElapsed) {}
+void RigidBodySystemSimulator::simulateTimestep(float timeStep)
+{
+	for (auto& rb : rigidBodies) {
+		rb.position += timeStep * rb.linearVelocity;
+		rb.linearVelocity += timeStep * rb.force / rb.mass;
+
+		rb.rotation = rb.rotation + (Quat(rb.angularVelocity.x, rb.angularVelocity.y, rb.angularVelocity.z, 0) * rb.rotation) * .5 * timeStep;
+
+		rb.rotation = rb.rotation.unit();
+		rb.angularMomentum += timeStep * rb.torque;
+		
+		Mat4 Rot = rb.rotation.getRotMat();
+		Mat4 RotT = Rot.inverse();
+		Mat4 tmp;
+		tmp.initScaling(rb.inverseInertia.x, rb.inverseInertia.y, rb.inverseInertia.z);
+		std::cout << rb.inverseInertia << "\n";
+		Mat4 Im1 = Rot * tmp * RotT;
+
+		rb.angularVelocity = Im1 * rb.angularMomentum;
+		std::cerr << rb.angularVelocity.x << "\n";
+
+		rb.force = Vec3(0.);
+		rb.torque = Vec3(0.);
+	}
+}
+
+void RigidBodySystemSimulator::onClick(int x, int y) {
+	if (m_iTestCase == 1)
+	{
+		applyForceOnBody(0, Vec3(.3, .5, .25), Vec3(1, 1, 0));
+	}
+}
+void RigidBodySystemSimulator::onMouse(int x, int y) {}
+
+// ExtraFunctions
+int RigidBodySystemSimulator::getNumberOfRigidBodies()
+{
+	return static_cast<int>(rigidBodies.size());
+}
+Vec3 RigidBodySystemSimulator::getPositionOfRigidBody(int i)
+{
+	if (i < getNumberOfRigidBodies())
+		return rigidBodies[i].position;
+	return Vec3();
+}
+Vec3 RigidBodySystemSimulator::getLinearVelocityOfRigidBody(int i)
+{
+	if (i < getNumberOfRigidBodies())
+		return rigidBodies[i].linearVelocity;
+	return Vec3();
+}
+Vec3 RigidBodySystemSimulator::getAngularVelocityOfRigidBody(int i)
+{
+	if (i < getNumberOfRigidBodies())
+		return rigidBodies[i].angularVelocity;
+	return Vec3();
+}
+void RigidBodySystemSimulator::applyForceOnBody(int i, Vec3 loc, Vec3 force)
+{
+	if (!(i < getNumberOfRigidBodies()))
+		return;
+	rigidBodies[i].force += force;
+	rigidBodies[i].torque += cross(loc - rigidBodies[i].position, force);
+
+}
+void RigidBodySystemSimulator::addRigidBody(Vec3 position, Vec3 size, int mass)
+{
+	RigidBody rb{};
+	rb.position = position;
+	rb.scale = size;
+	rb.mass = mass;
+	rb.inverseInertia = calcInvInertiaCube(rb.scale, rb.mass);
+	rigidBodies.push_back(rb);
+
+}
+void RigidBodySystemSimulator::setOrientationOf(int i, Quat orientation)
+{
+	if (!(i < getNumberOfRigidBodies()))
+		return;
+	rigidBodies[i].rotation = orientation.unit();
+}
+void RigidBodySystemSimulator::setVelocityOf(int i, Vec3 velocity)
+{
+	if (!(i < getNumberOfRigidBodies()))
+		return;
+	rigidBodies[i].linearVelocity = velocity;
+}
